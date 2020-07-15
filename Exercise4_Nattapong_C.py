db7938415910765461967604838c9d7a5a2d5491
Ariawa20 <66866066+Ariawa20@users.noreply.github.com>
2020-07-16 00:29:34 +0700

    Added Exercise4_Nattapong_C.py

---
+++ Exercise4_Nattapong_C.py
@@ -0,0 +1,11 @@
+eng = 55.45
+bis = 64.25
+ics = 75.52
+cpm = 15.55
+print("--- Your Score ---")
+print("Foundation English : ",eng)
+print("General Business : ",bis)
+print("Introduction to Computer Systems : ",ics)
+print("Computer Programming : ",cpm)
+y = "5"
+print(5+y)

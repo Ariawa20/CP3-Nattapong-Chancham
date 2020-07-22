6f03a3945a8900df8e886a62f2bbe71b026d8a47
Ariawa20 <66866066+Ariawa20@users.noreply.github.com>
2020-07-22 14:55:17 +0700

    Lecture_50_Nattapong_C.py

---
+++ Lecture_50_Nattapong_C.py
@@ -0,0 +1,13 @@
+def addNumber(x,y):
+    print(x+y)
+def deleteNumber(x,y):
+    print(x-y)
+def multiNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+
+addNumber(25,85)
+deleteNumber(25,85)
+multiNumber(25,85)
+divideNumber(25,85)
